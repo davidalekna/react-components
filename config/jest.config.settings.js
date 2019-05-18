81604d6e9609f8e064c476d7178ef69dcfe64d3d
David Alekna <alekna@live.co.uk>
2019-05-18 14:26:57 +0100

    configuration updates

---
+++ config/jest.config.settings.js
@@ -0,0 +1,25 @@
+const ignores = [
+  '/node_modules/',
+  '/fixtures/',
+  '/__tests__/helpers/',
+  '__mocks__',
+  '__stories__',
+];
+
+module.exports = {
+  transform: {
+    '.(ts|tsx)': 'ts-jest',
+  },
+
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+    },
+  },
+
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  testURL: 'http://localhost',
+
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.ts'],
+  testPathIgnorePatterns: [...ignores],
+};
