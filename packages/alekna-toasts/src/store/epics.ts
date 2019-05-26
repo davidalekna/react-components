61563df120dbadbaf45fe654bdabe0e6d79c5fc1
David Alekna <alekna@live.co.uk>
2019-05-26 09:37:33 +0100

    pause on individual toast

--- packages/alekna-toasts/src/store/epics.ts
+++ packages/alekna-toasts/src/store/epics.ts
@@ -1,25 +1,14 @@
-import { of, merge, interval, empty, concat } from 'rxjs';
+import { of, merge, interval, empty } from 'rxjs';
 import {
   filter,
-  delay,
   mergeMap,
-  takeUntil,
   mapTo,
   startWith,
   switchMap,
   scan,
   takeWhile,
-  tap,
-  mergeAll,
-  combineLatest,
 } from 'rxjs/operators';
-import {
-  CREATE,
-  DISMISS,
-  CLEAR_ALL,
-  MOUSE_ENTER,
-  MOUSE_LEAVE,
-} from './actions';
+import { CREATE, MOUSE_ENTER, MOUSE_LEAVE } from './actions';
 import { dismissToast, updateToast } from './actions';
 import { ofType } from './helpers';
 
@@ -32,8 +21,24 @@ export function createEpic(action$) {
       }
 
       const interval$ = interval(1000).pipe(mapTo(-1));
-      const pause$ = action$.pipe(ofType(MOUSE_ENTER)).pipe(mapTo(false));
-      const resume$ = action$.pipe(ofType(MOUSE_LEAVE)).pipe(mapTo(true));
+      const pause$ = action$
+        .pipe(
+          ofType(MOUSE_ENTER),
+          filter(({ payload }: any) => {
+            return payload === action.payload.id;
+          }),
+        )
+        .pipe(mapTo(false));
+      const resume$ = action$
+        .pipe(
+          ofType(MOUSE_LEAVE),
+          filter(({ payload }: any) => {
+            return payload === action.payload.id;
+          }),
+        )
+        .pipe(mapTo(true));
+
+      // TODO: update countdown on Toast
 
       return merge(pause$, resume$).pipe(
         startWith(true),
