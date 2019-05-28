8c97c6e8fcd5e85f51947546737eaa47f6b8b299
David Alekna <alekna@live.co.uk>
2019-05-28 19:37:24 +0100

    updates

--- packages/alekna-toasts/rollup.config.js
+++ packages/alekna-toasts/rollup.config.js
@@ -4,5 +4,9 @@ export default rollup({
   name: 'alekna-toasts',
   extraGlobals: {
     react: 'react',
+    'react-dom': 'react-dom',
+    'prop-types': 'prop-types',
+    'styled-components': 'styled-components',
+    'node_modules/react-dom/index.js': ['render', 'createPortal'],
   },
 });
